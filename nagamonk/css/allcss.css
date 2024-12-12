4f99d88132ef471e68cf6056da36b83e768dde9f
thanaphat jangmuewai <zzz056093@gmail.com>
2024-12-13 00:50:18 +0700

    1

---
+++ nagamonk/css/allcss.css
@@ -0,0 +1,15 @@
+.active{
+    background-color: #ddc767;
+    color: rgb(0, 0, 0);
+    float: left;
+    
+}
+.R{
+    float: right;
+}
+.naga {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
